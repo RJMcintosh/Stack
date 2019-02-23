40c920ecae90ba31b4d4c5d9d9ef8d7cb1675420
Rossspc <Rossspc@DESKTOP-C9UJ9T9>
2019-02-23 19:51:20 +0000

    New Commit

--- src/stack/StackAppication.java
+++ src/stack/StackAppication.java
@@ -13,36 +13,52 @@ import java.util.Scanner;
  */
 public class StackAppication {
     
+     
         public static void main(String[] args) {
-                 int numbers[] = {2,3,4,5};
-          
-                
-            Scanner stdin = new Scanner(System.in);
-                
-        ArrayStack a1 = new ArrayStack(numbers);
-        LinkedStack l1 = new LinkedStack();
+        Stack<String> s;
+Scanner stdin = new Scanner(System.in);
+          int numb;
       
-        int choice;
        
         System.out.println("Select what option you want to select");
         System.out.println("1 = Get Number, 2 = First Number, 3 = Find the biggest Number, 4 = Are they equal?, 5 = add new numbers, 6 = GCD,  7 = display all 0 = Exit");
- 
-        
-        
-        do {
-            System.out.print("Next action: ");
-            choice = stdin.nextInt();
+   for (int choice = 1; choice <= 2; choice++) {
             if (choice == 1) {
-                a1.push(1);
-                a1.pop();
+                s = new ArrayStack<>();
+            } else {
+                s = new LinkedStack<>();
+            }
+        
+  
+        do {
+            
+
+        
+            System.out.print("Next action: ");
+            numb = stdin.nextInt();
+            if (choice == 1) {
+                s.push("First");
+                System.out.println(s);
+                s.pop();
             } else if (choice == 2) {
-                l1.push(1);
-                l1.pop();
+                s.push("Second");
+                System.out.println(s);
+                s.pop();
+            }   else if (choice == 3) {
+                s.push("Three");
+                System.out.println(s);
+                s.pop();
+            }   else if (choice == 4) {
+                s.push("Four");
+                System.out.println(s);
+                s.pop();
             } 
              else if (choice != 0) {
                 System.out.println("Error: Valid choices are 0, 1 or 2");
             }
-        } while (choice != 0);
+        } while (numb != 0);
         System.out.println("Goodbye");
     }
+
+        }
 }
